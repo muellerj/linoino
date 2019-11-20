2da3708018d28e2a33cbd6d58e4caba63e2e5e9f
Jonas Mueller <jonas-mueller@mailbox.org>
2019-11-20 10:02:38 +0100

    Start refactoring voiceMenu()

--- lib/voice_menu.cpp
+++ lib/voice_menu.cpp
@@ -1,3 +1,13 @@
+void previewCurrentSelection(int previewFromFolder, int returnValue) {
+  do {
+    delay(10);
+  } while (isPlaying());
+  if (previewFromFolder == 0)
+    mp3.playFolderTrack(returnValue, 1);
+  else
+    mp3.playFolderTrack(previewFromFolder, returnValue);
+}
+
 int voiceMenu(int numberOfOptions, int startMessage, int messageOffset,
               bool preview = false, int previewFromFolder = 0) {
 
@@ -8,28 +18,26 @@ int voiceMenu(int numberOfOptions, int startMessage, int messageOffset,
   }
 
   while(true) {
+
     pauseButton.read();
     upButton.read();
     downButton.read();
+
     mp3.loop();
+
+    // Pause button
     if (pauseButton.wasPressed()) {
-      if (returnValue != 0)
-        return returnValue;
+      if (returnValue != 0) return returnValue;
       delay(1000);
     }
 
+    // Up button
     if (upButton.pressedFor(LONG_PRESS)) {
       returnValue = min(returnValue + 10, numberOfOptions);
       mp3.playMp3FolderTrack(messageOffset + returnValue);
       delay(1000);
       if (preview) {
-        do {
-          delay(10);
-        } while (isPlaying());
-        if (previewFromFolder == 0)
-          mp3.playFolderTrack(returnValue, 1);
-        else
-          mp3.playFolderTrack(previewFromFolder, returnValue);
+        previewCurrentSelection(previewFromFolder, returnValue);
       }
       ignoreUpButton = true;
     } else if (upButton.wasReleased()) {
@@ -38,30 +46,20 @@ int voiceMenu(int numberOfOptions, int startMessage, int messageOffset,
         mp3.playMp3FolderTrack(messageOffset + returnValue);
         delay(1000);
         if (preview) {
-          do {
-            delay(10);
-          } while (isPlaying());
-          if (previewFromFolder == 0)
-            mp3.playFolderTrack(returnValue, 1);
-          else
-            mp3.playFolderTrack(previewFromFolder, returnValue);
+          previewCurrentSelection(previewFromFolder, returnValue);
         }
-      } else
+      } else {
         ignoreUpButton = false;
+      }
     }
     
+    // Down button
     if (downButton.pressedFor(LONG_PRESS)) {
       returnValue = max(returnValue - 10, 1);
       mp3.playMp3FolderTrack(messageOffset + returnValue);
       delay(1000);
       if (preview) {
-        do {
-          delay(10);
-        } while (isPlaying());
-        if (previewFromFolder == 0)
-          mp3.playFolderTrack(returnValue, 1);
-        else
-          mp3.playFolderTrack(previewFromFolder, returnValue);
+        previewCurrentSelection(previewFromFolder, returnValue);
       }
       ignoreDownButton = true;
     } else if (downButton.wasReleased()) {
@@ -70,13 +68,7 @@ int voiceMenu(int numberOfOptions, int startMessage, int messageOffset,
         mp3.playMp3FolderTrack(messageOffset + returnValue);
         delay(1000);
         if (preview) {
-          do {
-            delay(10);
-          } while (isPlaying());
-          if (previewFromFolder == 0)
-            mp3.playFolderTrack(returnValue, 1);
-          else
-            mp3.playFolderTrack(previewFromFolder, returnValue);
+          previewCurrentSelection(previewFromFolder, returnValue);
         }
       } else {
         ignoreDownButton = false;
