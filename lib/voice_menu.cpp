24adec1a6cf68b62fb628678b66b83feff83c872
Jonas Mueller <jonas-mueller@mailbox.org>
2019-12-09 11:10:06 +0100

    Style

--- lib/voice_menu.cpp
+++ lib/voice_menu.cpp
@@ -10,39 +10,33 @@ void previewSelection(int folder, int track) {
   playTrack(folder, track);
 }
 
-int changeOption(int i, int delta, int optMin, int optMax, int previewFolder) {
+int changeOption(int i, int delta, int optMin, int optMax, int folder) {
   i = delta > 0 ? min(i + delta, optMax - optMin + 1) : max(i + delta, 1);
   playMessage((optMin - 1) + i);
   delay(1000);
-  previewSelection(previewFolder, i);
+  previewSelection(folder, i);
   return i;
 }
 
-int promptUserSelection(int question, int optMin, int optMax, 
-                        int previewFolder=-1) {
+int promptUserSelection(int question, int optMin, int optMax, int folder=-1) {
   int selection = 0;
+  int delta     = 0;
   playMessage(question);
 
   while(true) {
 
     mp3.loop();
+    delta = 0;
 
     switch(pollButtons()) {
-      case BTN_PAUSE_SHORTPRESS: 
-        if (selection != 0) return selection; 
-        break;
-      case BTN_UP_LONGPRESS: 
-        selection = changeOption(selection, +10, optMin, optMax, previewFolder);
-        break;
-      case BTN_UP_SHORTPRESS:
-        selection = changeOption(selection, +1, optMin, optMax, previewFolder);
-        break;
-      case BTN_DOWN_LONGPRESS: 
-        selection = changeOption(selection, -10, optMin, optMax, previewFolder);
-        break;
-      case BTN_DOWN_SHORTPRESS:
-        selection = changeOption(selection, -1, optMin, optMax, previewFolder);
-        break;
+      case BTN_PAUSE_SHORTPRESS: if (selection != 0) return selection; break;
+      case BTN_UP_LONGPRESS:     delta = +10; break;
+      case BTN_UP_SHORTPRESS:    delta =  +1; break;
+      case BTN_DOWN_LONGPRESS:   delta = -10; break;
+      case BTN_DOWN_SHORTPRESS:  delta =  -1; break;
     }
+
+    if (delta != 0)
+      selection = changeOption(selection, delta, optMin, optMax, folder);
   }
 }
