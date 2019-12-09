b339011d862f77e49feec51cfa88326844137913
Jonas Mueller <jonas-mueller@mailbox.org>
2019-12-09 14:35:18 +0100

    Rework interfaces

--- lib/track_navigation.h
+++ lib/track_navigation.h
@@ -7,13 +7,17 @@ uint16_t lastTrackFinished;
 
 bool isPlaying();
 void saveProgress();
-bool isPlaying();
+
 void nextTrack(uint16_t track);
+void previousTrack();
+
 void playTrack(uint8_t track);
 void playTrack(uint8_t folder, uint8_t track);
+
 void playMessage(uint16_t track);
+
 uint16_t getTrackCount();
-void previousTrack();
+
 void startPlayback();
 void pausePlayback();
 void resetPlayback(uint16_t track);
