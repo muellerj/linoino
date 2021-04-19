145d5717afcbfe59dc304064f935fe10e8ed3c07
Jonas Mueller <jonas@tigger.cloud>
2021-04-19 23:39:24 +0200

    Update DFMiniMp3 to 1.1.0

--- src/mp3_player.cpp
+++ src/mp3_player.cpp
@@ -1,6 +1,6 @@
 #include "headers.h"
 
-void Mp3Notify::OnError(uint16_t errorCode) {
+void Mp3Notify::OnError(DfMp3& mp3, uint16_t errorCode) {
   Serial.println("Com Error " + String(errorCode));
 }
 
@@ -11,27 +11,27 @@ void Mp3Notify::PrintlnSourceAction(DfMp3_PlaySources source, const char* action
   Serial.println(action);
 }
 
-void Mp3Notify::OnPlayFinished(DfMp3_PlaySources source, uint16_t track) {
+void Mp3Notify::OnPlayFinished(DfMp3& mp3, DfMp3_PlaySources _, uint16_t track) {
   if (!setupActive) {
     Serial.println("End of track " + String(track));
     nextTrack(track, false);
   }
 }
 
-void Mp3Notify::OnPlaySourceOnline(DfMp3_PlaySources source) {
+void Mp3Notify::OnPlaySourceOnline(DfMp3& mp3, DfMp3_PlaySources source) {
   PrintlnSourceAction(source, "online");
 }
 
-void Mp3Notify::OnPlaySourceInserted(DfMp3_PlaySources source) {
+void Mp3Notify::OnPlaySourceInserted(DfMp3& mp3, DfMp3_PlaySources source) {
   PrintlnSourceAction(source, "ready");
 }
 
-void Mp3Notify::OnPlaySourceRemoved(DfMp3_PlaySources source) {
+void Mp3Notify::OnPlaySourceRemoved(DfMp3& mp3, DfMp3_PlaySources source) {
   PrintlnSourceAction(source, "removed");
 }
 
 SoftwareSerial mySoftwareSerial(2, 3); // RX, TX
-DFMiniMp3<SoftwareSerial, Mp3Notify> mp3(mySoftwareSerial);
+DfMp3 mp3(mySoftwareSerial);
 
 void setupMp3Player() {
   mp3.begin();
--- src/mp3_player.h
+++ src/mp3_player.h
@@ -1,12 +1,15 @@
+class Mp3Notify;
+typedef DFMiniMp3<SoftwareSerial, Mp3Notify> DfMp3;
+
 class Mp3Notify {
   public:
-    static void OnError(uint16_t errorCode);
+    static void OnError(DfMp3& mp3, uint16_t errorCode);
     static void PrintlnSourceAction(DfMp3_PlaySources source, const char* action);
-    static void OnPlayFinished(DfMp3_PlaySources source, uint16_t track);
-    static void OnPlaySourceOnline(DfMp3_PlaySources source);
-    static void OnPlaySourceInserted(DfMp3_PlaySources source);
-    static void OnPlaySourceRemoved(DfMp3_PlaySources source);
+    static void OnPlayFinished(DfMp3& mp3, DfMp3_PlaySources source, uint16_t track);
+    static void OnPlaySourceOnline(DfMp3& mp3, DfMp3_PlaySources source);
+    static void OnPlaySourceInserted(DfMp3& mp3, DfMp3_PlaySources source);
+    static void OnPlaySourceRemoved(DfMp3& mp3, DfMp3_PlaySources source);
 };
 
-extern DFMiniMp3<SoftwareSerial, Mp3Notify> mp3;
+extern DfMp3 mp3;
 void setupMp3Player();
